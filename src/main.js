178db581b46e57eb1cb1b1106e14d2a55971e584
Simone <simone.zanni.87@gmail.com>
2024-07-14 11:17:05 +0200

    update character

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ k.loadAseprite("spritesheet", "./spritesheet.png", {
     sliceY: 31,
     anims: {
         "idle-down": 936,
+        "walk-down":{ from: 936, to: 939, loop: true, speed: 8},
+        "idle-side": 975,
+        "walk-side":{ from: 975, to: 978, loop: true, speed: 8},
+        "idle-up": 1014,
+        "walk-up":{ from: 1014, to: 1017, loop: true, speed: 8},
+
     }
 
 })
