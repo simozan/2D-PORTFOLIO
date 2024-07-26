fb613f6e1138b4b98edf9d984129f4fc4dda3572
Simone <simone.zanni.87@gmail.com>
2024-07-26 08:59:01 +0200

    update

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,24 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  pc: `This is my PC. My primary technical skills include the entire MERN stack, among others:
+
+<span style="color: red;">JavaScript ES6</span>,
+<span style="color: red;">React</span>,
+<span style="color: red;">NodeJS</span>,
+<span style="color: red;">MongoDB</span>,
+<span style="color: red;">Supabase</span>,
+<span style="color: red;">Express</span>,
+<span style="color: red;">HTML & CSS</span>,
+<span style="color: red;">Mongoose</span>,
+<span style="color: red;">Git</span>
+
+I am also familiar with <span style="color: red;">advanced CSS </span>and <span style="color: red;">Sass methodologies </span> like <span style="color: red;">BEM </span>and possess advanced knowledge of <span style="color: red;">responsive design</span> principles, <span style="color: red;">Flexbox</span>, and <span style="color: red;">Grid</span>.
+
+Anyway regardless of the language, I just like programming.
+
+Here is my <a href="https://github.com/simozan" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Ironhack certificate. I hung it on the wall because I'm proud of it.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
   I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
   You'll learn how I built this portfolio you're currently playing through!`,
@@ -13,11 +27,11 @@ export const dialogueData = {
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  Contact me at simone.zanni.87@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
   Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+  library: `There are a lot of books on my shelves. There is even one in French (I also speak French btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  exit: `If you want to exit Simone's portfolio, just close the tab.`,
 };
