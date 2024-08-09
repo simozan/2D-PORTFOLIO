768ee8f901b09db21a23a627e85d9de3acb7159c
Simone <simone.zanni.87@gmail.com>
2024-08-09 11:06:00 +0200

    update text

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,6 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. My primary technical skills include the entire MERN stack, among others:
-
 <span style="color: red;">JavaScript ES6</span>,
 <span style="color: red;">React</span>,
 <span style="color: red;">NodeJS</span>,
@@ -11,27 +10,25 @@ export const dialogueData = {
 <span style="color: red;">Express</span>,
 <span style="color: red;">HTML & CSS</span>,
 <span style="color: red;">Mongoose</span>,
-<span style="color: red;">Git</span>
-
+<span style="color: red;">Git</span>.
 I am also familiar with <span style="color: red;">advanced CSS </span>and <span style="color: red;">Sass methodologies </span> like <span style="color: red;">BEM </span>and possess advanced knowledge of <span style="color: red;">responsive design</span> principles, <span style="color: red;">Flexbox</span>, and <span style="color: red;">Grid</span>.
-
 Anyway regardless of the language, I just like programming.
-
 Here is my <a href="https://github.com/simozan" target="_blank">Github</a>!`,
-  "cs-degree": `This is my Ironhack certificate. I hung it on the wall because I'm proud of it.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
+
+  "cs-degree": `This is my Ironhack full stack web developer <a href="https://www.credential.net/ae6144f7-77e4-4256-b2f4-23280ecd7aac#gs.cq5bsa" target="_blank">certificate</a>. I hung it on the wall because I'm proud of it.`,
+
+  "sofa-table": `That's my sofa! Here, I like to relax, watch tv and play videogames 🎮!!`, 
+  
+  
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  bed: `Sleeping 8 hours every night is essential. It enhances creativity and productivity at work and helps maintain a good mood.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
   Contact me at simone.zanni.87@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
   Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  library: `In my library, you can find books written in many languages. In fact, my <span style="color: red;"> English </span> level is professional, as are my <span style="color: red;"> Spanish </span>, <span style="color: red;"> French </span>, and <span style="color: red;"> Catalan </span>. I also speak <span style="color: red;"> Italian </span>, which is my mother tongue.`,
   exit: `If you want to exit Simone's portfolio, just close the tab.`,
 };
