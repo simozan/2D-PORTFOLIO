f061777ccf2bdf09a036fd8f177ba649bf96fd22
Simone <simone.zanni.87@gmail.com>
2024-08-09 14:53:39 +0200

    update text

--- src/constants.js
+++ src/constants.js
@@ -17,17 +17,32 @@ Here is my <a href="https://github.com/simozan" target="_blank">Github</a>!`,
 
   "cs-degree": `This is my Ironhack full stack web developer <a href="https://www.credential.net/ae6144f7-77e4-4256-b2f4-23280ecd7aac#gs.cq5bsa" target="_blank">certificate</a>. I hung it on the wall because I'm proud of it.`,
 
-  "sofa-table": `That's my sofa! Here, I like to relax, watch tv and play videogames 🎮!!`, 
-  
-  
+  "sofa-table": `That's my sofa! Here, I like to relax, watch tv and play videogames 🎮!!`,
+
   tv: `This is a game-style portfolio was created using <span style="color: red;"> HTML</span>, <span style="color: red;"> CSS </span>, <span style="color: red;"> JavaScript </span>, and <span style="color: red;"> Kaboom.js</span>, a library for making games in JavaScript. The map was created with a program called <span style="color: red;"> Tiled </span>, a popular open-source map editor used for creating 2D game maps.
 It was created by following this tutorial: <a href="https://www.youtube.com/watch?v=gwtfWORCN0U" target="_blank">Here</a>!`,
   bed: `Sleeping 8 hours every night is essential. It enhances creativity and productivity at work and helps maintain a good mood.`,
+
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
   Contact me at simone.zanni.87@gmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+
+  projects: `Here are some of my projects: </br>
+  <a href="https://magnificent-gelato-243613.netlify.app/login" target="_blank">KIDDIE TASKS HEROES : Family Task Management Web App</a>
+Kiddie Tasks Heroes is a web app that helps families manage tasks, specifically teaching children about family responsibilities.
+The App is a group project created during the bootcamp in IRONHACK using
+JavaScript, HTML & CSS, MongoDB, React.js, Express.js, Node.js
+</br> </br>
+<a href="https://app-project.adaptable.app/" target="_blank">ALL YOU CAN TUPPER : Web App for Food Sharing</a>
+All You Can Tupper is a web app for sharing and requesting food. Users can post available food with pick-up details, and others can request and enjoy it.
+The App is a group project created during the bootcamp in IRONHACK using
+JavaScript, HTML & CSS, MongoDB, Mongooose, Express.js, Node.js
+</br> </br>
+<a href="https://simozan.github.io/EXTINCTION/" target="_blank">EXTINCTION : platform‑style video game</a>
+**Extinction** is a platform-style video game featuring a group of dinosaurs trying to escape from meteorites. The game is an individual project created during the bootcamp at Ironhack, using HTML, CSS, JavaScript, DOM manipulation, and Object-Oriented Programming (OOP).
+
+`,
+
   library: `In my library, you can find books written in many languages. In fact, my <span style="color: red;"> English </span> level is professional, as are my <span style="color: red;"> Spanish </span>, <span style="color: red;"> French </span>, and <span style="color: red;"> Catalan </span>. I also speak <span style="color: red;"> Italian </span>, which is my mother tongue.`,
+
   exit: `If you want to exit Simone's portfolio, just close the tab.`,
 };
